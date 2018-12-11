d4db50ee78d64904eccee9edb367a2d1f16a7ca2
Machado <Thiago.Machado@unisys.com>
2018-12-11 17:53:00 -0200

    atualização para fazer manutenção de renderização em casa.

--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { MoovieProvider } from '../../providers/moovie/moovie';
 
 /**
  * Generated class for the FeedPage page.
@@ -12,6 +13,9 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 @Component({
   selector: 'page-feed',
   templateUrl: 'feed.html',
+  providers: [
+       MoovieProvider
+  ]
 })
 export class FeedPage {
 
@@ -23,14 +27,28 @@ export class FeedPage {
     qntd_comments: 4,
     time_comment: "11h ago"
   }
+
+  public lista_filmes = new Array<any>();
   
   public nome_usuario:string = "Thiago Max";
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private moovieProvider: MoovieProvider) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad FeedPage');
+    this.moovieProvider.getLatestMovies().subscribe(
+      data=>{
+        const response = (data as any);
+        const objeto_retorno = JSON.parse(response._body);
+        this.lista_filmes = objeto_retorno.results;
+        console.log(objeto_retorno);
+      }, erro=>{
+        console.log(erro);
+      }
+    )
   }
 
 }
--- src/providers/moovie/moovie.ts
+++ src/providers/moovie/moovie.ts
@@ -9,13 +9,17 @@ import { Injectable } from '@angular/core';
 */
 @Injectable()
 export class MoovieProvider {
+  private baseApiPath = "https://api.themoviedb.org/3";
 
   constructor(public http: HttpClient) {
     console.log('Hello MoovieProvider Provider');
   }
 
   getLatestMovies() {
-    return this.http.get("http://.....")
+    return this.http.get(this.baseApiPath + "/movie/popular?api_key=" + this.getApiKey());
   }
 
+  getApiKey(): string{ 
+    return "025e22d628147e2a913cfc6fdc88c3bc";
+  }
 }
